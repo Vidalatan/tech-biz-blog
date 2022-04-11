42bbe3677bb3f527c8f9e3f8a5be059511437f76
Vidalatan <vidalatan@gmail.com>
2022-04-11 15:51:14 -0500

    Updated User.js model
    Added password hashing for creating and bulkcreating users

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {};
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+};
 
 User.init(
     {
@@ -20,6 +25,17 @@ User.init(
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            async beforeBulkCreate(newUserData) {
+              var newarr = newUserData.map( user => user.get({plain: true}))
+              for (user of newarr) { user.password = await bcrypt.hash(user.password, 10) }
+              return newarr;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
