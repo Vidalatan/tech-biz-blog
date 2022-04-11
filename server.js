fe62d37b79671cc31048eb2f27724d6e84a30e93
Vidalatan <vidalatan@gmail.com>
2022-04-11 16:07:48 -0500

    Updated server.js
    Added ability to utilize cookies

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const session = require('express-session');
+const handlebars = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
-const handlebars = require('express-handlebars')
-const hbs = handlebars.create({})
+const hbs = handlebars.create({});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sesh = {
+  secret: process.env.SESH_SECRET,
+  cookie: {
+    maxAge: 86400
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
+app.use(session(sesh));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`))
 
