2d8482f8c8f92158dc8e41d47f7fe2998a1fb2b1
Vidalatan <vidalatan@gmail.com>
2022-04-11 16:55:22 -0500

    Added user-routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body)
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(newUser);
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+})
+
+router.post('/login', async (req,res) => {
+    try {
+        const checkUser = await User.findOne({ where: { username: req.body.username } })
+        if (!checkUser) { 
+            res.status(404).json({ message: 'Incorrect username or password. Please try again...'}) 
+            return ;
+        }
+
+        const validPassword = await checkUser.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(404).json({ message: 'Incorrect username or password. Please try again...'})
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            console.log('~ file: user-routes.js ~ line 34 ~ req.session.save ~ req.session.cookie', req.session.cookie);
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+})
+
+router.post('/logout', (req,res) => {
+    req.session.loggedIn && req.session.destroy(() => { res.status(204).end() });
+    req.session.loggedIn || res.status(404).end();
+})
+
+module.exports = router;
